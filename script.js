86f574d2d2e81e34917a153d761fc2a9d22fe69f
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2023-12-21 16:28:44 -0300

    Adicionando a função async/await

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 //UL 
 const containerVideos = document.querySelector('.videos__container')
 
-const api = fetch('http://localhost:3000/videos')
-.then(res => res.json())
-.then( (videos)=> 
-   videos.forEach((video )=> {
 
-    containerVideos.innerHTML += `
-    <li class="videos__item">
+async function buscarVideosApi (){
+   try{
+    const buscar = await fetch('http://localhost:3000/videos');
+    const videos = await buscar.json();
     
-       <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+       videos.forEach((video )=> {
+           if(video.categoria === ''){
+               throw new Error("Esse video não tem categoria");
+           }
+          containerVideos.innerHTML += `
+            <li class="videos__item">
+    
+              <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
       
-       <div class='descricao-video' >
-          <img class='img-canal' src='${video.imagem}' alt='logo do canal' > 
+              <div class='descricao-video' >
+                 <img class='img-canal' src='${video.imagem}' alt='logo do canal' > 
 
-        <h3 class="titulo-video"> ${video.titulo} </h3>
-        <p class="titulo-canal">${video.descricao}</p>
-       </div>
-    </li>
-
-    `
+                 <h3 class="titulo-video"> ${video.titulo} </h3>
+                 <p class="titulo-canal">${video.descricao}</p>
+               </div>
+            </li>`
     ;
    })
-)
-.catch((error)=> {
-    containerVideos.innerHTML = `
-      <p> Houve um erro ao carregar os Videos : ${error}</p>
-    `
-})
-
+} catch (error){
+     containerVideos.innerHTML = `<p>Houve um erro ao carregar o video: ${error}</p>`
+}
+}
+buscarVideosApi()
