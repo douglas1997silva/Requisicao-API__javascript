ba8ed88e0fb3c3ce13dc0663932415e296fdd1e4
tom-log <nizekaos@gmail.com>
2023-12-30 00:34:38 -0300

    Refatoração da função buscarVideosApi para melhorar a segurança e legibilidade do código

--- script.js
+++ script.js
@@ -2,32 +2,54 @@
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarVideosApi() {
-   try {
-     // Lógica assíncrona que pode gerar exceções
-     const buscar = await fetch('http://localhost:3000/videos');
-     const videos = await buscar.json();
- 
-     videos.forEach((video) => {
-       if (video.categoria === '') {
-         throw new Error('Esse vídeo não tem categoria');
-       }
-       // Adicionei vídeos ao containerVideos
-       containerVideos.innerHTML += `
-         <li class="videos__item">
-           <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-           <div class='descricao-video'>
-             <img class='img-canal' src='${video.imagem}' alt='logo do canal'> 
-             <h3 class="titulo-video"> ${video.titulo} </h3>
-             <p class="titulo-canal">${video.descricao}</p>
-           </div>
-         </li>`;
-     });
-   } catch (error) {
-     // Tratamento de erro, se ocorrer
-     containerVideos.innerHTML = `<p>Houve um erro ao carregar o vídeo: ${error}</p>`;
-   }
- }
- 
- // Chamada da função buscarVideosApi
- buscarVideosApi();
- 
+  try {
+    const response = await fetch("http://localhost:3000/videos");
+    const videos = await response.json();
+
+    videos.forEach((video) => {
+      if (!video.categoria) {
+        throw new Error("Esse vídeo não tem categoria");
+      }
+
+      const li = document.createElement("li");
+      li.className = "videos item";
+
+      const iframe = document.createElement("iframe");
+      iframe.src = video.url;
+      iframe.title = video.titulo;
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute("allowfullscreen", "");
+
+      const divDescricao = document.createElement("div");
+      divDescricao.className = "descricao-video";
+
+      const imgCanal = document.createElement("img");
+      imgCanal.className = "img-canal";
+      imgCanal.src = video.imagem;
+      imgCanal.alt = "logo do canal";
+
+      const h3Titulo = document.createElement("h3");
+      h3Titulo.className = "titulo-video";
+      h3Titulo.textContent = video.titulo;
+
+      const pDescricao = document.createElement("p");
+      pDescricao.className = "titulo-canal";
+      pDescricao.textContent = video.descricao;
+
+      divDescricao.appendChild(imgCanal);
+      divDescricao.appendChild(h3Titulo);
+      divDescricao.appendChild(pDescricao);
+
+      li.appendChild(iframe);
+      li.appendChild(divDescricao);
+
+      document.getElementById("containerVideos").appendChild(li);
+    });
+  } catch (error) {
+    document.getElementById(
+      "containerVideos"
+    ).innerHTML = `<p>Houve um erro ao carregar o vídeo: ${error}</p>`;
+  }
+}
+
+buscarVideosApi();
