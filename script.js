a94821252a1479580085cc0b051aaeb675c95fd8
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2024-01-03 19:06:13 -0300

    FEAT"barra de pesquisa"

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarVideosApi() {
-   try {
-     // Lógica assíncrona que pode gerar exceções
-     const buscar = await fetch('http://localhost:3000/videos');
-     const videos = await buscar.json();
- 
-     videos.forEach((video) => {
-       if (video.categoria === '') {
-         throw new Error('Esse vídeo não tem categoria');
-       }
-       // Adicionei vídeos ao containerVideos
-       containerVideos.innerHTML += `
+  try {
+    // Lógica assíncrona que pode gerar exceções
+    const buscar = await fetch("http://localhost:3000/videos");
+    const videos = await buscar.json();
+
+    videos.forEach((video) => {
+      if (video.categoria === "") {
+        throw new Error("Esse vídeo não tem categoria");
+      }
+      // Adicionei vídeos ao containerVideos
+      containerVideos.innerHTML += `
          <li class="videos__item">
            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
            <div class='descricao-video'>
@@ -21,13 +21,37 @@ async function buscarVideosApi() {
              <p class="titulo-canal">${video.descricao}</p>
            </div>
          </li>`;
-     });
-   } catch (error) {
-     // Tratamento de erro, se ocorrer
-     containerVideos.innerHTML = `<p>Houve um erro ao carregar o vídeo: ${error}</p>`;
-   }
- }
- 
- // Chamada da função buscarVideosApi
- buscarVideosApi();
- 
+    });
+  } catch (error) {
+    // Tratamento de erro, se ocorrer
+    containerVideos.innerHTML = `<p>Houve um erro ao carregar o vídeo: ${error}</p>`;
+  }
+}
+
+// Chamada da função buscarVideosApi
+buscarVideosApi();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+
+//filtro de pesquisa
+function filtrarPesquisa() {
+  const video = document.querySelectorAll(".videos__item");
+
+  const videos = document.querySelectorAll(".videos__item");
+  const valorFiltro = barraDePesquisa.value.toLowerCase();
+
+  videos.forEach((video) => {
+    const titulo = video
+      .querySelector(".titulo-video")
+      .textContent.toLowerCase();
+
+    video.style.display = valorFiltro
+      ? titulo.includes(valorFiltro)
+        ? "block"
+        : "none"
+      : "block";
+  });
+}
